7817e1e665e4c199215981ce86c9248c5125aa81
cj3016358 <cj3016358@LAB01-PC06>
2023-03-16 11:11:38 -0300

    Adicionar arquivos de projeto.

---
+++ Lista2PR2/Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+            // Atividade 1
+
+            //double menos, mais, l, tot;
+
+            //menos = 0.35;
+            //mais = 0.28;
+
+            //Console.WriteLine("Quantas laranjas você irá comprar ? ");
+            //l = double.Parse(Console.ReadLine());
+
+            //if (l >= 12)
+            //    tot = l * mais;
+
+            //else
+            //    tot = l * menos;
+
+
+            //Console.WriteLine("Suas laranjas custarão " + tot);
+
+
+
+            // Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            // Atividade 2
+
+            //int idade, nasc;
+
+            //Console.WriteLine("Qual o ano de seu nascimento ? ");
+            //nasc = int.Parse(Console.ReadLine());
+
+            //idade = 2023 - nasc;
+
+            //if (idade >= 16)
+            //    Console.WriteLine("Voce pode votar ");
+
+            //else
+            //    Console.WriteLine("Voce não pode votar ");
+
+            //Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            // Atividade 3
+
+            //double l1, l2, l3;
+
+            //Console.WriteLine("Me diga o primeiro lado do triângulo");
+
+            //l1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Me diga o segundo lado do triângulo");
+
+            //l2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Me diga o terceiro lado do triângulo");
+            //l3 = double.Parse(Console.ReadLine());
+
+
+
+            // if (l1 == l2 && l2 == l3 && l3 == l1)
+
+            //     Console.WriteLine("É um triangulo é equilátero");    
+
+            //else if(l1 != l2 && l2 != l3 && l3 != l1)
+
+            //    Console.WriteLine("É um triangulo é escaleno");  
+
+            //else 
+
+            //        Console.WriteLine("É um triangulo é isosceles");
+
+            //Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            // Atividade 4
+
+            //double del, a, b, c, r1, r2;
+
+            //Console.WriteLine("Qual o valor de A");
+            //a = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Qual o valor de B");
+            //b = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Qual o valor de C");
+            //c = double.Parse(Console.ReadLine());
+
+            //del = (b * b) - 4 * a * c;
+            //r1 = ((-b + (Math.Sqrt(del))) / (2 * a));
+            //r2 = ((-b - (Math.Sqrt(del))) / (2 * a));
+
+
+            //Console.WriteLine("A raiz 1 é " + r1);
+            //Console.WriteLine("A raiz 2 é " + r2);
+
+            //Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            // Atividade 5
+
+            //int num;
+
+            //Console.WriteLine("Digite um número inteiro ");
+            //num = int.Parse(Console.ReadLine());
+
+            //if ((num % 3) == 0)
+            //    Console.WriteLine("Seu número é divisel por 3");
+
+            //else 
+            //    Console.WriteLine("Seu número não é divisel por 3");
+
+            //if ((num % 5) == 0)
+            //    Console.WriteLine("Seu número é divisel por 5");
+
+            //else
+            //    Console.WriteLine("Seu número não é divisel por 5");
+
+            //Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            // Atividade 6
+
+            //double a1, a2, a3;
+
+            //Console.WriteLine("Digite o primeiro angulo");
+            //a1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o segundo angulo");
+            //a2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o terceiro angulo");
+            //a3 = double.Parse(Console.ReadLine());
+
+            //if ((a1 == 90) || (a2 == 90) || (a3 == 90))
+            //    Console.WriteLine("Seu triângulo é um triângulo retângulo ");
+
+            //else if ((a1 > 90) || (a2 > 90) || (a3 > 90))
+            //    Console.WriteLine("Seu triângulo é um triângulo obtusângulo ");
+            //else
+            //    Console.WriteLine("Seu triângulo é um triângulo de três ângulos agudos");
+
+
+            //Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            // Atividade 7
+
+            //int n1, n2, n3;
+
+            //Console.WriteLine(" Digite tres numeros inteiros respectivamente ");
+            //n1 = int.Parse(Console.ReadLine());
+            //n2 = int.Parse(Console.ReadLine());
+            //n3 = int.Parse(Console.ReadLine());
+
+            //if ((n1 > n2) && (n1 > n3))
+            //    Console.WriteLine(n1);
+            //else if ((n2 < n1) && (n2 > n3))
+            //    Console.WriteLine(n3);
+            //else
+            //    Console.WriteLine(n2);
+
+            //Console.ReadKey();
+
+            //------------------------------------------------------------------------------------------------------------------------
+
+            //Ativide 8
+
+            //int ano;
+            //Console.WriteLine("Qual o ano ? ");
+            //ano = int.Parse(Console.ReadLine());
+
+            //if (ano % 400 == 0)
+            //{
+
+            //    Console.WriteLine("O ano " + ano + " é bissexto");
+            //}
+
+            //else
+            //{
+            //    if (ano % 4 == 0 && ano % 100 != 0)
+            //        Console.WriteLine("O ano " + ano + " é bissexto");
+
+            //    else
+            //    {
+            //        Console.WriteLine("O ano " + ano + "  é bissexto");
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            //Atividade 9
+
+            //int n1, n2;
+            //char car;
+
+            //Console.WriteLine("Digite dois numeros inteiros respectivamente");
+            //n1 = int.Parse(Console.ReadLine());
+            //n2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Isnsira o cararcter");
+            //car = char.Parse(Console.ReadLine());
+
+            //if (car == '+')
+            //    Console.WriteLine((("Você esta somando, e o resultado é ") + (n1 + n2)));
+            //else if (car == '-')
+            //    Console.WriteLine((("Você esta subtraindo, e o resultado é ") + (n1 - n2)));
+            //else if (car == '/')
+            //    Console.WriteLine((("Você esta dividindo, e o resultado é ") + (n1 / n2)));
+            //else if (car == '*')
+            //    Console.WriteLine((("Você esta multiplicando, e o resultado é ") + (n1 * n2)));
+            //else
+            //    Console.WriteLine("Esse caracter não e valido");
+
+            //Console.ReadKey();
+
+            // ------------------------------------------------ -------------------------------------------------- ----------------------
+
+            //Atividade 10
+            // Para gerar numeros aleatorios em C#,use a classe ramdom, para gerar um numero inteiro use a classe Next(minvalue, maxValue), para gerar um numero real use NextDouble() e ajuste valor multiplicando pelo tamnaho do intervalo e adicionando ao valor minimo.
+
+
+
+
+
+        }
+         }
+    }
+
